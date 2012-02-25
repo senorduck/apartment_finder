9fbaa4b5b4f0364db90d17dd662e78d0d29896a9
Ben Haley <bhaley@groupon.com>
2012-02-25 16:51:46 -0600

    Add styles on init. Add first filter and roughly define Filter class

--- coffee/benslist.coffee
+++ coffee/benslist.coffee
@@ -1,32 +1,106 @@
 class Filters
-	setDefaults: ->
-		this.formatPosts()
-		this.attachBehaviors()
-	
-	formatPosts: ->
-		@posts = $("body").find("p")
-		@posts.css 
-			"padding": "15px"
-			"background": "#eee"
-		@posts.append("<a href='#' class='hideLink' style='margin-left: 30px; color: #ccc;'>Hide All</a>")
+	postTag = "p"
+	posts = $("body").find(postTag)
+	fixedElems = ".bchead, blockquote:first"
+	bodyElems = "blockquote:eq(1)"
+	searchFields = "#searchfieldset"
 
-	attachBehaviors: ->
-		@hideLinks = $("a.hideLink")
-		@hideLinks.bind 'click', (event) ->
-			$(this).parent("p").hide()
+	setDefaults: ->
+		this.format()
+		# this.attachBehaviors()
+
+	defineFilters: ->
+		#add custom filter div to the page
+		$(searchFields).append("<div id='additional_search'>Custom Filters<div id='custom_form'></div></div>")
+
+		#open all links in a new tab
+		newTabFilter = new AttributeFilter("Links open in new tab", options =
+			{
+				element: $(posts).find("a:not(.hide_link)"),
+				attribute: {
+					name: 	"target",
+					orig: 	"_self",
+					custom: "_blank"
+				}
+			})
+	
+	format: ->
+		#wrap header elements in a fixed position div
+		$(fixedElems).wrapAll("<div id='fixed_header'></div>")
+
+		#move everything else down below the fixed header
+		$(bodyElems).addClass("under_fixed")
+
+		#style posts
+		posts.addClass("styled_post")
+
+		#add hide link to posts
+		# posts.append("<a href='#' class='hide_link'>Hide All</a>")
+
+		#add Filters div and initialize filters
+		this.defineFilters()
+
+	# attachBehaviors: ->
+		# hideLinks = $("a.hide_link")
+		# hideLinks.bind 'click', (event) ->
+		# 	$(this).parent(postTag).hide()
+
+class Filter
+	constructor: (@label, @options) ->
+		@name = @label.toLowerCase().replace /[ ]/g, ""
+		@checkbox = $("<input type='checkbox' name='" + @name + "' id='" + @name + "' \/>")
+		@elements = @options.element
+		this.apply()
+
+	apply: ->
+		$("#custom_form").append("<label for='" + @name + "'>" + @label + "<\/label>").append($(@checkbox))
+
+		$(@checkbox).bind 'click', (event) =>
+			if $(event.target).is(':checked')
+				this.toggleFilter(true)
+			else
+				this.toggleFilter(false)
+
+
+class AttributeFilter extends Filter
+	toggleFilter: (bool) =>
+		attributes = @options.attribute
+		if bool
+			value = attributes.custom
+		else
+			value = attributes.orig
+		$(@elements).attr(attributes.name, value)
+
 
 setFilters = ->
+	@initialized = true
 	pageFilters = new Filters()
 	pageFilters.setDefaults()
 
-v = "1.7.1"
-# if jquery is not already defined on the page, insert it into the page
-if window.jQuery == undefined || window.jQuery.fn.jquery < v
-	done = false
-	script = document.createElement("script")
-	script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js"
-	document.getElementsByTagName("head")[0].appendChild(script)
-	script.onload = =>
+initialize = ->
+	return if @initialized
+
+	#add some sweet styles
+	styles = document.createElement("link")
+	styles.type = "text/css"
+	styles.rel = "stylesheet"
+	styles.href = "http://localhost/apartment_finder/css/benslist.css"
+	document.getElementsByTagName("head")[0].appendChild(styles)
+
+	# if jquery is not already defined on the page, insert it into the page
+	if document.location.href.indexOf("localhost") < 0 && document.location.href.indexOf("craigslist.org") < 0
+		alert("For use with Craigslist apartment search pages.")
+		return
+	if window.jQuery == undefined || window.jQuery.fn.jquery < @version
+		script = document.createElement("script")
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + @version + "/jquery.min.js"
+		document.getElementsByTagName("head")[0].appendChild(script)
+		script.onload = =>
+			setFilters()
+	else
 		setFilters()
-else
-	setFilters()
+
+#set global setup variables, initialize
+@version = "1.7.1"
+@initialized ||= false
+initialize()
--- js/benslist.js
+++ js/benslist.js
@@ -1,54 +1,116 @@
 (function() {
-  var Filters, done, script, setFilters, v,
-    _this = this;
-
+  var AttributeFilter, Filter, Filters, initialize, setFilters;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    if(typeof parent.extended == "function") parent.extended.call(parent, child);
+    return child;
+  };
   Filters = (function() {
-
+    var bodyElems, fixedElems, postTag, posts, searchFields;
     function Filters() {}
-
+    postTag = "p";
+    posts = $("body").find(postTag);
+    fixedElems = ".bchead, blockquote:first";
+    bodyElems = "blockquote:eq(1)";
+    searchFields = "#searchfieldset";
     Filters.prototype.setDefaults = function() {
-      this.formatPosts();
-      return this.attachBehaviors();
+      return this.format();
     };
-
-    Filters.prototype.formatPosts = function() {
-      this.posts = $("body").find("p");
-      this.posts.css({
-        "padding": "15px",
-        "background": "#eee"
-      });
-      return this.posts.append("<a href='#' class='hideLink' style='margin-left: 30px; color: #ccc;'>Hide All</a>");
-    };
-
-    Filters.prototype.attachBehaviors = function() {
-      this.hideLinks = $("a.hideLink");
-      return this.hideLinks.bind('click', function(event) {
-        return $(this).parent("p").hide();
+    Filters.prototype.defineFilters = function() {
+      var newTabFilter, options;
+      $(searchFields).append("<div id='additional_search'>Custom Filters<div id='custom_form'></div></div>");
+      return newTabFilter = new AttributeFilter("Links open in new tab", options = {
+        element: $(posts).find("a:not(.hide_link)"),
+        attribute: {
+          name: "target",
+          orig: "_self",
+          custom: "_blank"
+        }
       });
     };
-
+    Filters.prototype.format = function() {
+      $(fixedElems).wrapAll("<div id='fixed_header'></div>");
+      $(bodyElems).addClass("under_fixed");
+      posts.addClass("styled_post");
+      return this.defineFilters();
+    };
     return Filters;
-
   })();
-
+  Filter = (function() {
+    function Filter(label, options) {
+      this.label = label;
+      this.options = options;
+      this.name = this.label.toLowerCase().replace(/[ ]/g, "");
+      this.checkbox = $("<input type='checkbox' name='" + this.name + "' id='" + this.name + "' \/>");
+      this.elements = this.options.element;
+      this.apply();
+    }
+    Filter.prototype.apply = function() {
+      $("#custom_form").append("<label for='" + this.name + "'>" + this.label + "<\/label>").append($(this.checkbox));
+      return $(this.checkbox).bind('click', __bind(function(event) {
+        if ($(event.target).is(':checked')) {
+          return this.toggleFilter(true);
+        } else {
+          return this.toggleFilter(false);
+        }
+      }, this));
+    };
+    return Filter;
+  })();
+  AttributeFilter = (function() {
+    __extends(AttributeFilter, Filter);
+    function AttributeFilter() {
+      this.toggleFilter = __bind(this.toggleFilter, this);
+      AttributeFilter.__super__.constructor.apply(this, arguments);
+    }
+    AttributeFilter.prototype.toggleFilter = function(bool) {
+      var attributes, value;
+      attributes = this.options.attribute;
+      if (bool) {
+        value = attributes.custom;
+      } else {
+        value = attributes.orig;
+      }
+      return $(this.elements).attr(attributes.name, value);
+    };
+    return AttributeFilter;
+  })();
   setFilters = function() {
     var pageFilters;
+    this.initialized = true;
     pageFilters = new Filters();
     return pageFilters.setDefaults();
   };
-
-  v = "1.7.1";
-
-  if (window.jQuery === void 0 || window.jQuery.fn.jquery < v) {
-    done = false;
-    script = document.createElement("script");
-    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-    document.getElementsByTagName("head")[0].appendChild(script);
-    script.onload = function() {
+  initialize = function() {
+    var script, styles;
+    if (this.initialized) {
+      return;
+    }
+    styles = document.createElement("link");
+    styles.type = "text/css";
+    styles.rel = "stylesheet";
+    styles.href = "http://localhost/apartment_finder/css/benslist.css";
+    document.getElementsByTagName("head")[0].appendChild(styles);
+    if (document.location.href.indexOf("localhost") < 0 && document.location.href.indexOf("craigslist.org") < 0) {
+      alert("For use with Craigslist apartment search pages.");
+      return;
+    }
+    if (window.jQuery === void 0 || window.jQuery.fn.jquery < this.version) {
+      script = document.createElement("script");
+      script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + this.version + "/jquery.min.js";
+      document.getElementsByTagName("head")[0].appendChild(script);
+      return script.onload = __bind(function() {
+        return setFilters();
+      }, this);
+    } else {
       return setFilters();
-    };
-  } else {
-    setFilters();
-  }
-
+    }
+  };
+  this.version = "1.7.1";
+  this.initialized || (this.initialized = false);
+  initialize();
 }).call(this);
